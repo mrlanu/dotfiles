6ffe87c47d93b2825c188de60690c2a1cfa3b076
mrlanu <mrlanu@gmail.com>
2022-04-05 14:56:50 -0500

    init

---
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -0,0 +1,52 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+	return
+end
+
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+  local s = server.name
+  print("Server - ", s)
+  local opts = {
+		on_attach = require("user.lsp.handlers").on_attach,
+		capabilities = require("user.lsp.handlers").capabilities,
+	}
+
+	 if server.name == "jsonls" then
+	 	local jsonls_opts = require("user.lsp.settings.jsonls")
+	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+	 end
+
+	 if server.name == "sumneko_lua" then
+	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+	 end
+
+	 if server.name == "pyright" then
+	 	local pyright_opts = require("user.lsp.settings.pyright")
+	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+	 end
+
+	 --if server.name == "rust_analyzer" then
+	 	--local rust_opts = require("user.lsp.settings.rust_tools")
+	 	--opts = vim.tbl_deep_extend("force", rust_opts, opts)
+    --require("rust-tools").setup(opts)
+    --print("Hello")
+    --return
+	 --end
+  --
+	-- This setup() function is exactly the same as lspconfig's setup function.
+	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+	--print("Hello 2")
+  server:setup(opts)
+end)
