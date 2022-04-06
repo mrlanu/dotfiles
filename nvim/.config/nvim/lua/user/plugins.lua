3f41abec960e70fc64b61b2880b5d879746fe534
mrlanu <mrlanu@gmail.com>
2022-04-06 15:21:56 -0500

    toggleterm

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -45,7 +45,10 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
-
+  
+  -- Terminal
+  use "akinsho/toggleterm.nvim"
+  
   -- Colorschemes
   use "lunarvim/colorschemes"
   --use "lunarvim/darkplus.nvim"
