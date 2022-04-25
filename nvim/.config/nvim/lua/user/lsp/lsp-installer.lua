0258bd3d7b9c176bc861f4932abcaca6459bb2ea
mrlanu <mrlanu@gmail.com>
2022-04-25 11:55:58 -0500

    fix rust_tools

--- nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -3,20 +3,10 @@ if not status_ok then
 	return
 end
 
-
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-  local s = server.name
-  print("Server - ", s)
+
   local opts = {
 		on_attach = require("user.lsp.handlers").on_attach,
 		capabilities = require("user.lsp.handlers").capabilities,
@@ -37,16 +27,15 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	 end
 
-	 --if server.name == "rust_analyzer" then
-	 	--local rust_opts = require("user.lsp.settings.rust_tools")
-	 	--opts = vim.tbl_deep_extend("force", rust_opts, opts)
-    --require("rust-tools").setup(opts)
-    --print("Hello")
-    --return
-	 --end
-  --
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	--print("Hello 2")
+	 if server.name == "rust_analyzer" then
+    local rust_tools_opts = require("user.lsp.settings.rust_tools")
+	 	opts = vim.tbl_deep_extend("force", rust_tools_opts, opts)
+    -- Initialize the LSP via rust-tools instead
+        require("rust-tools").setup {
+            server = vim.tbl_deep_extend("force", server:get_default_options(), opts),
+        }
+        server:attach_buffers()
+    return
+	 end
   server:setup(opts)
 end)
